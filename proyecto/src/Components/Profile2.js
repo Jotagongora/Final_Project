b8756403900ae55a93088c3117fbef2f0eb0d54b
Jota Amd <gongorarealjota@gmail.com>
2021-06-05 19:51:04 +0200

    Pagina perfil usuario 99% acabada

--- proyecto/src/Components/Profile2.js
+++ proyecto/src/Components/Profile2.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Profile2.css';
-import Avatar from '../Images/avatar_prueba.jpg';
-import BgImg from '../Images/imagenfondo.jpg';
 
 export default function Profile2() {
     return (
-        <div className="profileContainer">
-            <div className="profileHeader">
+        <div>
+            <div className="profileContainer">
                 <div className="box1">
 
                 </div>
-                <div className="box2">
+                <div className="box2 bg-img">
                     
                 </div>
                 <div className="box3">
@@ -20,6 +18,35 @@ export default function Profile2() {
             <div className="profileImg"></div> {/*TODO: Arreglar margenes de foto perfil} */}
             <div>
                 <h1 className="textSize">Juan Alberto</h1>
+            </div>
+            <div className="navContainer">
+                <div className="box1"></div>
+                <nav className="profileNav box2">
+                    <a href="" className="isActive">Publicaciones</a>
+                    <a href="">Fotos</a>
+                    <a href="">Biblioteca</a>
+                    <button>Editar perfil</button>
+                </nav>
+                <div className="box3"></div>
+            </div>
+            <div className="navContainer">
+                <div className="profileAsideLeft"></div>
+                <div className="box2">
+                    <div className="post borderPost">
+                        <div className="postImg"></div>
+                        <div>
+                            <h1>Título publicación</h1>
+                            <p>Fecha</p>
+                        </div>
+                    </div>
+                    <div className="borderPost">
+                        <div className="toAttachImg"></div>
+                    </div>
+                    <div className="DownBorderPost">
+                        <p>El nuevo juego. El nuevo juego. El nuevo juego. El nuevo juego. El nuevo juego. El nuevo juego. El nuevo juego. El nuevo juego. asdasd</p>
+                    </div>
+                </div>
+                <div className="profileAsideRight"></div>
             </div>
         </div>
     )
