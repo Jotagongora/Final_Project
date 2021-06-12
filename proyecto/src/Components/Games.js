e1aeb2d447ed8f360f927249a13217a34050d3bc
Jota Amd <gongorarealjota@gmail.com>
2021-06-12 13:28:30 +0200

    Metida pagina Games con fetch a la API

--- proyecto/src/Components/Games.js
+++ proyecto/src/Components/Games.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import {useEffect} from 'react';
 import {GlobalContext} from '../Navbar';
-import './Game.css';
+import './Games.css';
 
 export default function Games() {
 
@@ -18,12 +18,33 @@ export default function Games() {
     
 
     return (
-        <div>
-            {games.map((game, index) => {
-                return (
-                    <div className="gameImg" style={{backgroundImage: `url(${game.background_image})`}}></div>
-                )
-            })}
+        <div className="allGamesBlock">
+            <div className="searchInput">
+                <h1>Juegos</h1>
+                <input type="search" placeholder="Buscar juegos..."/>
+                <i className="fas fa-search"></i>
+            </div>
+            <div className="gamesContainer">
+                {games.map((game, index) => {
+                    return (
+                        <div  className="gameContainer">
+                            <div className="gameImg" style={{backgroundImage: `url(${game.background_image})`}}></div>
+                            <div  className="gameDescription">
+                                <h3>{game.name}</h3>
+                                <ul>
+                                    <li><button>Likes</button><button>Publicaciones</button></li>
+                                    <li><span>Fecha de lanzamiento: </span><span>{game.released}</span></li>
+                                    <li><span>Género: </span><span>{game.genres.map((genre, index)=> {
+                                        return (
+                                            <span>{genre.name} </span>
+                                        )
+                                    })}</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
